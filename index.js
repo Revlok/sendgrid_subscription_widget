e4cef26126f0cdc8eb6860a30cd7dd0c24c4a10f
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-18 19:13:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const router = require('./server/router');
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 router(app);
 
 // Server setup
